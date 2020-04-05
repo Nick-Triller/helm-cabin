3554550e44fecadf16af637e9f0c4c94e3bd18b2
Nick Triller <NickTriller@gmail.com>
2020-04-05 13:15:36 +0200

    Introduce magefile and linting

---
+++ Magefile.go
@@ -0,0 +1,70 @@
+//+build mage
+
+package main
+
+import (
+	"os"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+// BuildDockerImage builds all artifacts in a docker multi stage build
+// and results in a Docker Image containing the artifacts
+func BuildDockerImage() error {
+	return sh.RunV("docker", "build", "-f", "build/Dockerfile", "-t", "nicktriller/helm-cabin:latest", ".")
+}
+
+// BuildBackend locally runs the backend with `go run`.
+// This target is supposed to be run in development only and assumes tiller is
+// reachable at 127.0.0.1:44134
+func RunBackend() error {
+	return sh.RunV("go", "run", "cmd/helmcabin/main.go", "--tillerAddress", "127.0.0.1:44134")
+}
+
+// BuildServerAll locally builds the artifacts for all supported platforms
+func BuildServerAll() error {
+	if err := BuildServerWindows(); err != nil {
+		return err
+	}
+	return BuildServerLinux()
+}
+
+// BuildWindows locally builds the artifacts for windows
+func BuildServerWindows() error {
+	return buildLocal("windows", "amd64")
+}
+
+// BuildWindows locally builds the artifacts for linux
+func BuildServerLinux() error {
+	return buildLocal("linux", "amd64")
+}
+
+// Lint lints the go code
+func Lint() error {
+	return sh.RunV("golint", "./...")
+}
+
+// CI runs targets are that supposed to run in continous integration pipelines
+func CI() error {
+	if err := BuildServerAll(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// BuildFrontend locally builds the frontend
+func BuildFrontend() error {
+	return sh.RunV("npm", "--prefix", "web", "run", "build")
+}
+
+func buildLocal(goos string, goarch string) error {
+	mg.Deps(Lint)
+	_ = os.Setenv("GOOS", goos)
+	_ = os.Setenv("GOARCH", goarch)
+	out := "bin/helmcabin"
+	if goos == "windows" {
+		out += ".exe"
+	}
+	return sh.RunV("go", "build", "-o", out, "./cmd/helmcabin")
+}
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/imdario/mergo v0.3.7 // indirect
 	github.com/jmoiron/sqlx v1.2.0 // indirect
 	github.com/lib/pq v1.2.0 // indirect
+	github.com/magefile/mage v1.9.0
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
 	github.com/pilu/fresh v0.0.0-20190826141211-0fa698148017 // indirect
